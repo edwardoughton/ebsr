3a51b1e37be261a1e51db1a5dc6d7ba926c54d71
edwardoughton <edward.oughton@gmail.com>
2022-06-29 08:51:30 -0400

    Update inputs

--- scripts/inputs.py
+++ scripts/inputs.py
@@ -8,18 +8,20 @@ April 2022.
 """
 
 PARAMETERS = {
-    'confidence_level': 95,
-    'active_users_perc': 5,
-    'market_share_perc': 25,
-    'busy_hour_traffic_perc': 15,
+    'confidence_level': 90,
+    'active_users_perc': 10,
+    # 'market_share_perc': 25,
+    # 'cells_per_site_2G': 3,
+    # 'cells_per_site_4G': 3,
+    'busy_hour_traffic_perc': 10,
     'dl_spectrum_GHz_hic': 30,
     'dl_spectrum_GHz_umc': 20,
     'dl_spectrum_GHz_lmc': 20,
     'dl_spectrum_GHz_lic': 10,
-    'traffic_per_user_gb_hic': 20,
-    'traffic_per_user_gb_umc': 15,
-    'traffic_per_user_gb_lmc': 10,
-    'traffic_per_user_gb_lic': 5,
+    'traffic_per_user_gb_hic': 30,
+    'traffic_per_user_gb_umc': 30,
+    'traffic_per_user_gb_lmc': 30,
+    'traffic_per_user_gb_lic': 30,
     'urban_rural_pop_density_km2': 500,
 }
 
